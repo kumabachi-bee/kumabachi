3f82c4f288b1c29df48ba27b11033e3f92db20b4
KatTakahashi <t.ka.ka.t.11@gmail.com>
2022-04-25 08:29:16 +0000

    [Add]events routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  # =============== 管理者用(URLに/admin/を付与する為、namespace) ===============
+  namespace :admin do
+    resources :events, except:[:index, :show]
+  end
+
+  # =============== 会員用(URLに/public/不要な為、scope module) ===============
   devise_for :members
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope module: :public do
+    resources :events, only:[:index, :show]
+  end
 end
