c61e0919ab697522cc3924036957c4eb80109ab5
KatTakahashi <t.ka.ka.t.11@gmail.com>
2022-04-25 08:22:17 +0000

    [Add]association and validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,12 @@
 class Event < ApplicationRecord
+  # --------------- アソシエーション ---------------
+  has_many :reserves, dependent: :destroy
+  
+  # --------------- バリデーション ---------------
+  validates :date, presence: true
+  validates :limit, presence: true
+  validates :is_available, presence: true
+  validates :category, presence: true
+  validates :title, presence: true
+  validates :body, presence: true
 end
