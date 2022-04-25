57b1f9d86917cf9b1dfc81c7bc199a558bb02638
KatTakahashi <t.ka.ka.t.11@gmail.com>
2022-04-25 08:41:49 +0000

    [Add]action contents

--- app/controllers/public/events_controller.rb
+++ app/controllers/public/events_controller.rb
@@ -1,7 +1,11 @@
 class Public::EventsController < ApplicationController
+  # --------------- 投稿一覧ページ --------------
   def index
+    @events = Event.all
   end
-  
+
+  # --------------- 投稿詳細ページ --------------
   def show
+    @event = Event.find(params[:id])
   end
 end
