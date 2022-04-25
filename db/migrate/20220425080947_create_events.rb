082f8f0d95e12d4572bbd682edd4466464aefaeb
KatTakahashi <t.ka.ka.t.11@gmail.com>
2022-04-25 08:16:43 +0000

    [Add]events columns

--- db/migrate/20220425080947_create_events.rb
+++ db/migrate/20220425080947_create_events.rb
@@ -1,7 +1,12 @@
 class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
-
+      t.datetime :date
+      t.integer :limit
+      t.boolean :is_available
+      t.integer :category
+      t.string :title
+      t.text :body
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_25_033139) do
+ActiveRecord::Schema.define(version: 2022_04_25_080947) do
+
+  create_table "events", force: :cascade do |t|
+    t.datetime "date"
+    t.integer "limit"
+    t.boolean "is_available"
+    t.integer "category"
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "members", force: :cascade do |t|
     t.string "email", default: "", null: false
