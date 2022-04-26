858167bdd4108b07e620f4c72edeea8caee81d0d
makiko_1996 <t_makiko_1996@yahoo.co.jp>
2022-04-26 11:40:41 +0000

    [fix]devise_admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,13 @@ class ApplicationController < ActionController::Base
       root_path
     end
   end
+  
+  # ログアウト後のリダイレクト先
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin
+      new_admin_session_path
+    else
+      root_path
+    end
+  end
 end
