aef0258cf1fb3391a56c85553c99840a1f97cf9d
KatTakahashi <t.ka.ka.t.11@gmail.com>
2022-04-26 13:03:39 +0000

    [Add]events post, edit, delete function

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # =============== 管理者用(URLに/admin/を付与する為、namespace) ===============
   namespace :admin do
-    resources :events, except:[:index, :show]
+    # resources :events, except:[:index, :show]
+    resources :events, only:[:create, :new, :update, :edit]
   end
 
   # =============== 会員用(URLに/public/不要な為、scope module) ===============
@@ -9,7 +10,7 @@ Rails.application.routes.draw do
   scope module: :public do
     root 'homes#top'
     get 'about_me' => 'homes#about_me'
-    resources :events, only:[:index, :show]
+    resources :events, only:[:index, :show, :destroy]
     resources :contacts, only:[:new, :create]
   end
 end
