d90d5e02d5663f7b7e68280d8916e503e5e00802
makiko_1996 <t_makiko_1996@yahoo.co.jp>
2022-04-26 08:01:40 +0000

    create_admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery
+  
+  protected
+  
+  # ログイン後のリダイレクト先
+  def after_sign_in_path_for(resource_or_scope)
+    if resource.is_a?(Admin)
+      new_admin_event_path
+    else
+      root_path
+    end
+  end
 end
